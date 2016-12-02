b1783b9b751d2d3a747e965ccef05c7fa005b5be
Eric Fredine <eric.fredine@gmail.com>
2016-12-02 00:21:48 +0000

    Install express, socket.io and verify operation.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+
+  componentDidMount() {
+    var socket = io('http://localhost:8080');
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
+  }
+
   render() {
     return (
       <h1>Hello React :)</h1>
---
+++ server.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
