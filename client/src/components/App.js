6890c26a87b1d55eb93f760822ff59bc030e62d5
Eric Fredine <eric.fredine@gmail.com>
2016-12-02 18:50:17 +0000

    Put socket set-up back into App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react';
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
-const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
 
-export default App
+class App extends Component {
+
+  componentDidMount() {
+    var socket = io('http://localhost:8080');
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    );
+  }
+}
+export default App;
+
+// const App = () => (
+//   <div>
+//     <AddTodo />
+//     <VisibleTodoList />
+//     <Footer />
+//   </div>
+// )
+
+// export default App
