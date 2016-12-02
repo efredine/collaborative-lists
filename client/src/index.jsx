cda37b0e859e36936e98936c2941ba5847343c09
Eric Fredine <eric.fredine@gmail.com>
2016-12-02 00:49:02 +0000

    Install redux and react-redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,8 +4,25 @@
 require("../styles/application.scss");
 
 // Render the top-level React component
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<App />, document.getElementById('react-root'));
+
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+// import todoApp from './reducers'
+// import App from './components/App'
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+let store = createStore(() => {})
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+)
