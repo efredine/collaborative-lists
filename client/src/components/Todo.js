0dee9d700e2c9a624c5a50c75652981a96a3615f
Eric Fredine <eric.fredine@gmail.com>
2016-12-18 18:12:57 +0000

    Update prop types of Todo component.

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -11,10 +11,10 @@ const Todo = ({ onClick, completed, content }) => (
   </div>
 )
 
-// Todo.propTypes = {
-//   onClick: PropTypes.func.isRequired,
-//   completed: PropTypes.bool.isRequired,
-//   text: PropTypes.string.isRequired
-// }
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  content: PropTypes.object.isRequired
+}
 
 export default Todo
