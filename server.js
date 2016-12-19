7ed9b010e25734e00af0fa77feb496bda788c35d
Eric Fredine <eric.fredine@gmail.com>
2016-12-19 21:47:09 +0000

    Remove in-memory version of action history.

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
-let nextTodoId = 0;
-const actionHistory = [];
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
@@ -71,25 +69,16 @@ const actionHelpers = require('./lib/actionHelpers')(knex);
 io.on('connection', function(socket){
   console.log("Socket connected: " + socket.id);
   socket.on('action', (action) => {
+    console.log('INCOMING ACTION:');
     console.log(action);
-    switch(action.type) {
-    case'SERVER/ADD_CARD':
-      action.type = 'ADD_CARD';
-      break;
-    case 'SERVER/TOGGLE_CARD':
-      action.type = 'TOGGLE_CARD';
-      break;
-    case 'SERVER/MOVE_CARD':
-      action.type = 'MOVE_CARD';
-      break;
-    default:
-      break;
-    }
-    console.log(action);
-    // actionHistory.push(action);
+    // Actions are of the form SERVER/<ACTION>.  The SERVER portion of this is stripped
+    // before broadcasting to all the clients.
+    action.type = action.type.split('/')[1];
     actionHelpers.insert(1, 1, action)
     .then(id => {
       action.id = id;
+      console.log('BROADCAST ACTION');
+      console.log(action);
       io.emit('action', action);
     })
   });
