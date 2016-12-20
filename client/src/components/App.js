0a5e70e7a61a6a7a7182fe0e7501b0b7752b5f38
Eric Fredine <eric.fredine@gmail.com>
2016-12-20 04:00:09 +0000

    Install react-bootstrap and create a sample navbar.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
 import MovieContainer from '../containers/movieContainers.jsx'
-import NavBar from './NavBar.jsx';
+import LoginContainer from '../containers/LoginContainer.js';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
+const NavbarInstance = () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem eventKey={1} href="#">Link</NavItem>
+      <NavItem eventKey={2} href="#">Link</NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <LoginContainer/>
+  </Navbar>
+);
 
 class App extends Component {
 
   render() {
     return(
       <div>
-        <NavBar/>
+        <NavbarInstance/>
         <div className="panel-container">
           <MovieContainer/>
         </div>
