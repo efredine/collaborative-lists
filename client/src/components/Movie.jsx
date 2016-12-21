7e20643b479032f234948f6e3b0dd0477e9064bd
Nathan Eyerley <eyerleyn@gmail.com>
2016-12-21 02:14:24 +0000

    added state change switches to middle main list section. Now the panel will display the proper button for remove/add/ or undo depending on its state and will react accordingly. A few css changes

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
-import { Collapse, Button, Well, ProgressBar } from 'react-bootstrap';
+import { Collapse, Button, Well, ProgressBar, Glyphicon } from 'react-bootstrap';
 
 class Movie extends Component {
 
-constructor(...args) {
+  constructor(...args) {
     super(...args);
 
     this.state = {};
+  }
+
+  renderAddRemove() {
+    const { completed } = this.props;
+    console.log("Completed:", completed);
+    if (completed === false) {
+      return (
+        <div className="remove">
+          <img onClick={this.onRemove} src="http://localhost:8080/images/remove.png"/>
+        </div>
+      );
+    }
+    if (completed === true) {
+      return (
+        <div className="undo">
+          <Glyphicon onClick= {this.onClick} glyph="glyphicon glyphicon-repeat"/>
+        </div>
+      );
+    }
+     if (completed === undefined) {
+      return (
+        <div className="add">
+          <img onClick={this.onAdd} src="http://localhost:8080/images/add.png"/>
+        </div>
+      );
+    }
   }
 
   onAdd = () => {
@@ -15,23 +41,27 @@ constructor(...args) {
   }
 
   onRemove = () => {
-    const {onRemove, index} = this.props;
-    onRemove(index);
+    const {onClick, index} = this.props;
+    onClick();
+  }
+
+  onClick = () => {
+    const {onClick, index} = this.props;
+    onClick();
   }
 
   render() {
+
     const {original_title, vote_average, overview, poster_path} = this.props.content;
 
     return(
       <div>
       <div className="panel-movie panel panel-default">
         <div className="panel-heading" onClick={ ()=> this.setState({ open: !this.state.open })}>
-          <div className="remove">
-            <img onClick={this.onRemove} src="http://localhost:8080/images/remove.png"/>
-          </div>
           <h3 className="panel-title">
             {original_title}
           </h3>
+          {this.renderAddRemove()}
         <Collapse in={this.state.open}>
           <div>
             <Well>
@@ -45,9 +75,6 @@ constructor(...args) {
             </Well>
           </div>
         </Collapse>
-          <div className="add">
-            <img onClick={this.onAdd} src="http://localhost:8080/images/add.png"/>
-          </div>
         </div>
         <div className="panel-body">
           <p> Rating: PG-13 | Genre: Action &amp; Adventure </p>
