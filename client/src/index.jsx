fa42e97470cb0a8533691c2c50db5146ee598990
Eric Fredine <eric.fredine@gmail.com>
2016-12-21 02:50:19 +0000

    Add a middleware filter that discards notifications for events that don't apply to the currently active list.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,13 +10,14 @@ import reducer from './reducers'
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 import createLogger from 'redux-logger';
+import notifications from './middleware/notifications';
 import ReduxThunk from 'redux-thunk';
 
 
 const loggerMiddleware = createLogger();
 const socket = io('http://localhost:8080');
 const socketIoMiddleware = createSocketIoMiddleware(socket, ['SERVER/ADD_CARD', 'SERVER/TOGGLE_CARD', 'SERVER/MOVE_CARD'], pessimisticExecute);
-const store = applyMiddleware(ReduxThunk, socketIoMiddleware, loggerMiddleware)(createStore)(reducer);
+const store = applyMiddleware(ReduxThunk, socketIoMiddleware, notifications, loggerMiddleware)(createStore)(reducer);
 
 function pessimisticExecute(action, emit, next, dispatch) {
   emit('action', action);
