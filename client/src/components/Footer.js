63cc2a3a45ff651d7634c0b9b7d54c98132795b2
Eric Fredine <eric.fredine@gmail.com>
2016-12-21 02:31:19 +0000

    Change default visibility filter to ACTIVE and re-arrange order of filters on the bottom.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,16 +5,16 @@ const Footer = () => (
   <p>
     Show:
     {" "}
-    <FilterLink filter="SHOW_ALL">
-      All
-    </FilterLink>
-    {", "}
     <FilterLink filter="SHOW_ACTIVE">
       Active
     </FilterLink>
     {", "}
+    <FilterLink filter="SHOW_ALL">
+      All
+    </FilterLink>
+    {", "}
     <FilterLink filter="SHOW_COMPLETED">
-      Completed
+      Completed/Deleted
     </FilterLink>
   </p>
 )
