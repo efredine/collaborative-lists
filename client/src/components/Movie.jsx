a20a75cd876533c8271913e6bb938690dbae37b4
Nathan Eyerley <eyerleyn@gmail.com>
2016-12-21 03:51:00 +0000

    added drop down icon button that rotates depending on if panel is collapsed or not

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -9,31 +9,48 @@ class Movie extends Component {
     this.state = {};
   }
 
+  drop () {
+    if (this.state.open === true) {
+      return (
+        <div className="drop">
+          <Glyphicon onClick={ ()=> this.setState({ open: !this.state.open })} glyph="glyphicon glyphicon-chevron-down "/>
+        </div>
+      );
+    }else {
+      return (
+        <div className="drop">
+          <Glyphicon onClick={ ()=> this.setState({ open: !this.state.open })} glyph="glyphicon glyphicon-chevron-right"/>
+        </div>
+      );
+    }
+  }
+
   renderAddRemove() {
     const { completed } = this.props;
     console.log("Completed:", completed);
     if (completed === false) {
       return (
-        <div className="remove">
-          <img onClick={this.onRemove} src="http://localhost:8080/images/remove.png"/>
-        </div>
+          <div className="remove">
+            <img onClick={this.onRemove} src="http://localhost:8080/images/remove.png"/>
+          </div>
       );
     }
     if (completed === true) {
       return (
-        <div className="undo">
-          <Glyphicon onClick= {this.onClick} glyph="glyphicon glyphicon-repeat"/>
-        </div>
+          <div className="undo">
+            <Glyphicon onClick= {this.onClick} glyph="glyphicon glyphicon-repeat"/>
+          </div>
       );
     }
      if (completed === undefined) {
-      return (
-        <div className="add">
-          <img onClick={this.onAdd} src="http://localhost:8080/images/add.png"/>
-        </div>
+        return (
+            <div className="add">
+              <img onClick={this.onAdd} src="http://localhost:8080/images/add.png"/>
+            </div>
       );
     }
   }
+
 
   onAdd = () => {
     const {onAdd, index} = this.props;
@@ -57,24 +74,25 @@ class Movie extends Component {
     return(
       <div>
       <div className="panel-movie panel panel-default">
-        <div className="panel-heading" onClick={ ()=> this.setState({ open: !this.state.open })}>
-          <h3 className="panel-title">
+        <div className="panel-heading">
+          <h3 className="panel-title" onClick={ ()=> this.setState({ open: !this.state.open })}>
             {original_title}
           </h3>
-          {this.renderAddRemove()}
-        <Collapse in={this.state.open}>
-          <div>
-            <Well>
-            <div className="poster">
-              <img src={"http://image.tmdb.org/t/p/w185/" + poster_path}/>
-            </div>
-              <p>{overview}</p>
-              <div>
-               <ProgressBar bsStyle="danger" active now={vote_average * 10} label={`${vote_average} / 10 Average Rating`}/>
+            <Collapse in={this.state.open}>
+            <div>
+              <Well>
+              <div className="poster">
+                <img src={"http://image.tmdb.org/t/p/w185/" + poster_path}/>
               </div>
-            </Well>
-          </div>
-        </Collapse>
+                <p>{overview}</p>
+                <div>
+                 <ProgressBar bsStyle="danger" active now={vote_average * 10} label={`${vote_average} / 10 Average Rating`}/>
+                </div>
+              </Well>
+            </div>
+          </Collapse>
+          {this.renderAddRemove()}
+          {this.drop()}
         </div>
         <div className="panel-body">
           <p> Rating: PG-13 | Genre: Action &amp; Adventure </p>
