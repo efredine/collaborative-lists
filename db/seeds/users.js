5dd8665283f4c2873b0b8ea352a4a19a87a78ff8
Eric Fredine <eric.fredine@gmail.com>
2017-01-11 22:58:50 +0000

    Update seeds to use project team member names.

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({name: 'Alice'}),
-        knex('users').insert({name: 'Bob'}),
-        knex('users').insert({name: 'Charlie'})
+        knex('users').insert({name: 'Eric'}),
+        knex('users').insert({name: 'Nathan'}),
+        knex('users').insert({name: 'Faisal'})
       ]);
     });
 };
