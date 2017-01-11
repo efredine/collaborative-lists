1c54f9371565e38da2eede707c147e9c793248ab
Eric Fredine <eric.fredine@gmail.com>
2017-01-11 02:25:25 +0000

    Add a sort control to the bottom of the list container.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import FilterLink from '../containers/FilterLink'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, NavItem, Glyphicon } from 'react-bootstrap'
 
 const Footer = () => (
-  <Nav bsStyle="pills" pullRight>
-    <FilterLink filter="SHOW_COMPLETED">
-      Completed/Deleted
-    </FilterLink>
-    <FilterLink filter="SHOW_ALL">
-      All
-    </FilterLink>
-    <FilterLink filter="SHOW_ACTIVE">
-      Active
-    </FilterLink>
-  </Nav>
+  <div>
+    <Nav bsStyle="pills" pullLeft>
+      <NavItem>
+       <Glyphicon onClick={ () => {} } glyph="glyphicon glyphicon-sort"/>
+      </NavItem>
+    </Nav>
+    <Nav bsStyle="pills" pullRight>
+      <FilterLink filter="SHOW_COMPLETED">
+        Deleted
+      </FilterLink>
+      <FilterLink filter="SHOW_ACTIVE">
+        Active
+      </FilterLink>
+    </Nav>
+  </div>
 )
 
 export default Footer
