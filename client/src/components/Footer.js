6fa09c588a3065faefe96f5b811fc87cda58dd13
Eric Fredine <eric.fredine@gmail.com>
2017-02-12 17:21:02 +0000

    Button to add an item to a list.  Not connected to anything yet.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import FilterLink from '../containers/FilterLink'
-import { Nav, NavItem } from 'react-bootstrap'
+import { Nav, NavItem, Button, Glyphicon } from 'react-bootstrap'
 import SortControl from '../containers/SortControl'
 
 const Footer = () => (
   <div>
     <Nav bsStyle="pills" pullLeft>
       <SortControl/>
-    </Nav>
-    <Nav bsStyle="pills" pullRight>
       <FilterLink filter="SHOW_COMPLETED">
         Deleted
       </FilterLink>
       <FilterLink filter="SHOW_ACTIVE">
         Active
       </FilterLink>
+    </Nav>
+    <Nav bsStyle="pills" pullRight>
+      <Button bsStyle="danger" className="btn-circle btn-lg list-btn-add">
+        <Glyphicon glyph="plus"/>
+      </Button>
     </Nav>
   </div>
 )
